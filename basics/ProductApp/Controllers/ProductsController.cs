ac86a7d934d33d80ab444a715ecb4060791eba4b
Dilay Berin Akkan <121443116+dilayberin@users.noreply.github.com>
2024-05-19 16:19:22 +0300

    Add files via upload

---
+++ basics/ProductApp/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ILogger<ProductsController> _logger;
+        public ProductsController(ILogger<ProductsController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = new List<Product>()
+            {
+               new Product() { Id = 1,ProductName="computer"},
+               new Product() { Id = 2,ProductName="keyboard"},
+               new Product() { Id = 3,ProductName="mouse"}
+            };
+            _logger.LogInformation("getallproducts actions has been called.");
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult GetAllProducts([FromBody] Product product)
+        {
+
+            _logger.LogWarning("product has been created.");
+            return StatusCode(201);
+        }
+    }
+
+    
+}
+
+
